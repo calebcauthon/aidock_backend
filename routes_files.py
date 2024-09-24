71cf3e2d15226951d6e98504e312f6da9fdb9ce1
caleb cauthon <caleb.cauthon@pricefx.com>
2024-09-23 20:47:28 -0500

    Rename superuser_routes to files_routes and update app registration accordingly.

--- routes_files.py
+++ routes_files.py
@@ -3,9 +3,9 @@ from routes_auth_helpers import platform_admin_required
 from db.file_model import FileModel
 from db.organization_model import OrganizationModel
 
-superuser_routes = Blueprint('files', __name__)
+files_routes = Blueprint('files', __name__)
 
-@superuser_routes.route('/files')
+@files_routes.route('/files')
 @platform_admin_required
 def list_files():
     organizations = OrganizationModel.get_all_organizations()
@@ -13,7 +13,7 @@ def list_files():
     files = FileModel.get_all_files()
     return render_template('superuser_ui/files.html', organizations=organizations_dict, files=files)
 
-@superuser_routes.route('/files/delete/<int:file_id>', methods=['POST'])
+@files_routes.route('/files/delete/<int:file_id>', methods=['POST'])
 @platform_admin_required
 def delete_file(file_id):
     if FileModel.delete_file(file_id):
